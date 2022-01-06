beef2961ac4d8ca65828beac210a4cf067ee1291
faustaadp <faustaadp@gmail.com>
2022-01-06 14:05:54 +0700

    init

---
+++ src/test/java/com/example/interngdp/InternGdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interngdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternGdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
