ee19dda2d561de250ec6b3c56d865728049ee9bd
faustaadp <faustaadp@gmail.com>
2022-01-07 13:44:35 +0700

    init

---
+++ src/main/java/com/example/interngdp/InternGdpApplication.java
@@ -0,0 +1,13 @@
+package com.example.interngdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternGdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternGdpApplication.class, args);
+    }
+
+}
